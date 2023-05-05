86da2473bb2bb285596150a3ed5d66d8758246cb
Dimitrios Tsiakmakis <dimitrios.tsiakmakis@santannapisa.it>
2023-05-05 18:00:03 +0200

    add transforms for 3d DICOM images

--- src/larynx/data/make_dataset.py
+++ src/larynx/data/make_dataset.py
@@ -1,22 +1,31 @@
-from pathlib import Path
 import glob
 import os
-import numpy as np
+import sys
 import SimpleITK as sitk  # noqa: N813
 import numpy as np
-import itk
+import logging
+
+# import itk
 from monai.transforms import SaveImage
 from monai.visualize import matshow3d
 from monai.data import PILReader
+from monai.data import CacheDataset, DataLoader, Dataset
+from monai.utils import first, set_determinism# import itk
+from monai.transforms import SaveImage
+from monai.visualize import matshow3d
+from monai.data import PILReader
+from monai.data import CacheDataset, DataLoader, Dataset
+from monai.utils import first, set_determinism
+from monai.transforms import LoadImage
 
-from larynx.data.transforms import get_tranforms
+from larynx.data.transforms import get_transforms
 from larynx.utils.config import Config
 
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
 def get_images_from_folder(project_path=None, folder_name='304'):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-        
-        Gets the data from ../../data/raw
+    """ 
+        Gets the data paths from data/raw/ + folder_name
     """
     config = Config()
     if project_path is None:
@@ -24,13 +33,15 @@ def get_images_from_folder(project_path=None, folder_name='304'):
 
     dir = os.path.join(config.data_raw_path, folder_name)
     train_images = sorted(glob.glob(os.path.join(dir, "*")))
-    
-    print(len(train_images))
+
+    logging.warning('GET IMAGES FROM: ' + dir + ', len: ' + str(len(train_images)))
 
     return train_images
 
 
-def save_image(img, folder_name):
+def save_image(image, folder_name):
+    """Save image to data/processed/ + folder_name.
+    """
     pth = os.path.join(Config().data_processed_path, folder_name)
     saver = SaveImage(
         output_dir=pth,
@@ -41,19 +52,41 @@ def save_image(img, folder_name):
         writer="ITKWriter",
         separate_folder=False
     )
-    saver.set_options(data_kwargs={"spatial_ndim": 2})
-    img = saver(img)
+    # saver.set_options(data_kwargs={"spatial_ndim": 2})
+    saver(image)
 
+
+def save_images(img_list: list, folder_name: str):
+    for img in img_list:
+        save_image(img, folder_name=folder_name)
+
+
+def get_dataloaders(first_n=None):
+    """Returns a TRAIN dataloader"""
+    train_images = get_images_from_folder(folder_name='304')
+    if not train_images:
+        logging.warning('Watch out! There are no data in the specific folder')
+        logging.critical('checjk')
+        exit()
+    if first_n is None:
+        first_n = -int(0.2*len(train_images))  # 20% split ratio Train/Val
+    data_dicts = [{"image": image_name, "label": label_name} for image_name, label_name in zip(train_images, train_images)]
+    train_files, val_files =  data_dicts[:first_n], data_dicts[first_n:]
+
+    train_transforms, val_transforms = get_transforms()
+    set_determinism(seed=0)
+
+    train_ds = CacheDataset(data=train_files, transform=train_transforms, cache_rate=0, num_workers=4)
+    # train_ds = Dataset(data=train_files, transform=train_transforms)
+    train_loader = DataLoader(train_ds, batch_size=3, shuffle=True, num_workers=4)
+
+    return train_loader
 
 
 if __name__ == '__main__':
-    config = Config()
-    images = get_images_from_folder(folder_name='304')
-    transform = get_tranforms()
+    train_loader = get_dataloaders(first_n=2)
 
-    test_data = {"image": images[0]}
-    result = transform(test_data)
-
-    print(f"image data shape:{result['image'].shape}")
-
-    save_image(img=result["image"], folder_name='304')
+    check_data = first(train_loader)
+    print(check_data["image"].shape)
+    print(type(check_data["image"]))
+    save_image(check_data["image"], folder_name='mico')
--- src/larynx/data/transforms.py
+++ src/larynx/data/transforms.py
@@ -1,14 +1,59 @@
-from monai.transforms import LoadImage, LoadImaged, Resized, Compose, SaveImage
-import numpy as np
+from monai.transforms import (LoadImaged, 
+                              Resized, 
+                              Compose,
+                              EnsureChannelFirstd, 
+                              CropForegroundd, 
+                              Orientationd, 
+                              RandCropByPosNegLabeld,
+                              ScaleIntensityRanged,
+                              Spacingd,
+                              )
 
 
-def get_tranforms():
-    transform = Compose(
-            [
-                LoadImaged(keys="image", image_only=True, ensure_channel_first=True, dtype=np.uint8),
-                Resized(keys="image", spatial_size=[512, 512, -1]),
-            ]
-        )
-    
-    return transform
+def get_transforms():
+    train_transforms = Compose(
+        [
+            LoadImaged(keys=["image", "label"]),
+            EnsureChannelFirstd(keys=["image", "label"]),
+            ScaleIntensityRanged(
+                keys=["image"],
+                a_min=-200,
+                a_max=400,
+                b_min=0.0,
+                b_max=1.0,
+                clip=True,
+            ),
+            CropForegroundd(keys=["image", "label"], source_key="image"),
+            Orientationd(keys=["image", "label"], axcodes="RPI"),
+            Spacingd(keys=["image", "label"], pixdim=(1.05, 1.05, 1), mode=("nearest")),
+            RandCropByPosNegLabeld(
+                keys=["image", "label"],
+                label_key="label",
+                spatial_size=(96, 96, 1),
+                pos=1,
+                neg=1,
+                num_samples=4,
+                image_key="image",
+                image_threshold=0,
+            ),
+        ]
+    )
+    val_transforms = Compose(
+        [
+            LoadImaged(keys=["image", "label"]),
+            EnsureChannelFirstd(keys=["image", "label"]),
+            ScaleIntensityRanged(
+                keys=["image"],
+                a_min=-200,
+                a_max=400,
+                b_min=0.0,
+                b_max=1.0,
+                clip=True,
+            ),
+            CropForegroundd(keys=["image", "label"], source_key="image"),
+            Orientationd(keys=["image", "label"], axcodes="RPI"),
+            Spacingd(keys=["image", "label"], pixdim=(1.05, 1.05, 1), mode=("nearest")),
+        ]
+    )
+    return train_transforms, None
 
